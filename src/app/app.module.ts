fd6147638b542db5787b47edbcbe965b9b99878c
David Morales <5dmorales474@gmail.com>
2021-11-06 20:50:52 +0100

    First commit (with components base created)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { SceneComponent } from './components/scene/scene.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SceneComponent
   ],
   imports: [
     BrowserModule
