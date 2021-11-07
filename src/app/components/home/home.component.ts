fef8d6666c5782923609dead45b50cb5bf3cb4f1
David Morales <5dmorales474@gmail.com>
2021-11-07 01:03:02 +0100

    Level 1 and 2 (forgot commit at level 1)

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -12,4 +12,17 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  displayChild: boolean = false;
+
+  goToApp(): void {
+    this.displayChild = true;
+  }
+
+  phrases: string[] = [
+    "El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial",
+    "Sentia curiositat per l'interior de la nau i es va posar a inspeccionar-la. Va arribar a una sala amb dues portes.",
+    "L'heroi va decidir travessar la porta que el portava a casa",
+    "Mentrestant, altres heroes no van tenir tanta sort en la seva elecció ..."
+  ]
+
 }
--- src/app/components/scene/scene.component.ts
+++ src/app/components/scene/scene.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-scene',
@@ -12,4 +12,28 @@ export class SceneComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  @Input() phrases: string[] = [];
+
+  currentSentence: number = 0;
+
+  prev(): void {
+    const newSentence: number = this.currentSentence - 1;
+
+    if (newSentence < 0) {
+      this.currentSentence = this.phrases.length - 1;
+    } else {
+      this.currentSentence = newSentence;
+    }
+  }
+
+  next(): void {
+
+    const newSentence: number = this.currentSentence + 1;
+
+    if (newSentence >= this.phrases.length) {
+      this.currentSentence = 0;
+    } else {
+      this.currentSentence = newSentence;
+    }
+  }
 }
